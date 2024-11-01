94cbd66e4f2e49be5ae62c6b6e12354d022e421a
Niraj <nirajtiwari1506@gmail.com>
2024-11-01 10:14:59 +0530

    added search functinality as well as reset search, using useState hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import * as XYZ from "./Components/Header";
-// import Body1 from "./Components/Body1";
+import Body1 from "./Components/Body1";
 import Footer from "./Components/Footer";
 
 import HeaderComponent, { Title } from "./Components/Header"; //importing named and default export on the same line.
@@ -144,78 +144,6 @@ const burgerKing = {
 // };
 
 //destructuring is being used here
-const RestaurantCard = ({ name, imageUrl, cuisine, rating }) => {
-  // const { name, cuisine, rating, imageUrl } = restaurant; //instead of doing this we can also use restaurant. any property as well.
-  return (
-    <div className="card">
-      <img className="card-img" src={imageUrl} alt="Burger King" />
-      <div className="card-content">
-        <h2 className="card-title">{name}</h2>
-        <p className="card-category">{cuisine}</p>
-        <p className="card-rating">{rating}</p>
-      </div>
-    </div>
-  );
-};
-const restaurantList = [
-  {
-    id: 1,
-    name: "Burger King",
-    cuisine: "American, Fast Food",
-    rating: 4.2,
-    imageUrl:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/cab874d5-c7ed-4122-9eb9-935992f4bcee_253596.JPG",
-  },
-  {
-    id: 2,
-    name: "Pizza Hut",
-    cuisine: "Pizza, Italian",
-    rating: 4.1,
-    imageUrl:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/7/16/c997ac50-07f4-4dff-b1d6-03ace6065734_14780.jpg",
-  },
-  {
-    id: 3,
-    name: "Starbucks",
-    cuisine: "Cafe, Beverages",
-    rating: 4.5,
-    imageUrl:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/097bc34fa9627e3bc442c9a254887970",
-  },
-  {
-    id: 4,
-    name: "Domino's Pizza",
-    cuisine: "Pizza, Italian",
-    rating: 4.3,
-    imageUrl:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/10/19/2d369c2d-429a-4ab1-a853-b6e07a08e59f_745868.jpg",
-  },
-  {
-    id: 5,
-    name: "Subway",
-    cuisine: "Healthy, Sandwiches",
-    rating: 4.0,
-    imageUrl:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/RX_THUMBNAIL/IMAGES/VENDOR/2024/10/25/5fd4d719-cc0a-4af8-a0a0-4af11898f0df_36014.jpg",
-  },
-  {
-    id: 6,
-    name: "McDonald's",
-    cuisine: "Fast Food, Burgers",
-    rating: 4.4,
-    imageUrl:
-      "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e585619e8994c449cb84884a2b998f98",
-  },
-];
-const Body1 = () => {
-  return (
-    <div className="body1">
-      {restaurantList.map((restaurant, index) => (
-        <RestaurantCard key={index} {...restaurant} />
-      ))}
-    </div>
-  );
-};
 
 const AppLayout = () => {
   return (
--- src/Components/Body1.js
+++ src/Components/Body1.js
@@ -1,10 +1,49 @@
 import { restaurantList } from "./Constants";
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+
+function filterRestaurants(searchText, restaurants) {
+  const result = restaurants.filter((rest) =>
+    rest.name.toLowerCase().includes(searchText.toLowerCase())
+  );
+  return result;
+}
+
 const Body1 = () => {
+  const [searchText, setSearchText] = useState("KFC");
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  function handleClick(e) {
+    setSearchText(e.target.value);
+  }
   return (
     <div className="body1">
-      {restaurantList.map((restaurant, index) => (
-        <RestaurantCard />
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="search"
+          onChange={handleClick}
+        ></input>
+        <button
+          className="search-btn"
+          onClick={() => {
+            const data = filterRestaurants(searchText, restaurants);
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+        <button
+          className="search-btn"
+          onClick={() => {
+            setRestaurants(restaurantList);
+          }}
+        >
+          Reset
+        </button>
+      </div>
+      {restaurants.map((restaurant, index) => (
+        <RestaurantCard {...restaurant} />
       ))}
     </div>
   );
